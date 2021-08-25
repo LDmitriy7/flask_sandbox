333dca3ebdd9de698111b0fa0bc25903c25be609
Dmitriy <levchenko.d.a1998@gmail.com>
2021-08-25 16:46:40 +0300

    fix port

--- aiohttp_app.py
+++ aiohttp_app.py
@@ -22,10 +22,12 @@ def main():
     try:
         ssl_context = ssl.SSLContext()
         ssl_context.load_cert_chain(config.SSL_CERT_FILE, config.SSL_KEY_FILE)
+        port = 443
     except (OSError, TypeError):
         ssl_context = None
+        port = 80
 
-    web.run_app(app, ssl_context=ssl_context)
+    web.run_app(app, ssl_context=ssl_context, port=port)
 
 
 if __name__ == '__main__':
