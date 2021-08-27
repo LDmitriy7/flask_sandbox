585ef7c8fd3adac456d41451185ab4f03bdd65d6
Dmitriy <levchenko.d.a1998@gmail.com>
2021-08-27 23:22:50 +0300

    .

--- config.py
+++ config.py
@@ -6,3 +6,7 @@ load_dotenv()
 
 SSL_CERT_FILE = os.getenv('SSL_CERT_FILE')
 SSL_KEY_FILE = os.getenv('SSL_KEY_FILE')
+DB_USERNAME = os.getenv('DB_USERNAME')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_AUTH_SOURCE = os.getenv('DB_AUTH_SOURCE', 'admin')
+TOKEN = os.getenv('TOKEN')
--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,62 @@
-from flask import Flask
+import mongoengine as me
+from flask import Flask, redirect, request, url_for
+
+import config
 
 app = Flask(__name__)
+
+me.connect(
+    db='my-bots',
+    username=config.DB_USERNAME,
+    password=config.DB_PASSWORD,
+    authentication_source=config.DB_AUTH_SOURCE,
+)
+
+
+class Redirect(me.Document):
+    key: str = me.StringField(unique=True)
+    src: str = me.StringField()
 
 
 @app.route('/')
 def index():
     return 'Hello, world!'
+
+
+@app.route('/redirect/<key>')
+def redirect_(key: str):
+    redirect_doc = Redirect.objects(key=key).first()
+
+    if redirect_doc is None:
+        return 'Redirect not found.'
+
+    return redirect(redirect_doc.src)
+
+
+@app.route('/redirects/add')
+def add_redirect():
+    required_args = ('key', 'src', 'token')
+
+    try:
+        key, src, token = (request.args[i] for i in required_args)
+    except:
+        route = url_for('add_redirect')
+        args = "&".join(f'{a}=.' for a in required_args)
+        return f'{route}?{args}'
+
+    if token != config.TOKEN:
+        return 'Wrong token.'
+
+    redirect_doc = Redirect.objects(key=key).first()
+
+    if redirect_doc is None:
+        Redirect(key=key, src=src).save()
+    else:
+        redirect_doc.src = src
+        redirect_doc.save()
+
+    redirect_url = url_for('redirect_', key=key)
+    return f'<a href="{redirect_url}" >{key}</a>'
 
 
 if __name__ == '__main__':
